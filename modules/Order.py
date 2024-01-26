915f85257eaa01eb890a0ec792da4383ba1fce0e
Виталий Юдин <viudin@astralinux.ru>
2024-01-26 12:33:28 +0200

    delivery fee multipliers added

--- modules/Order.py
+++ modules/Order.py
@@ -16,7 +16,7 @@ class Order(BaseModel):
         print("FEE count")
         max_fee = 1500
 
-        value = 10
+        value = 0
         value = self._multiplicators(value)
         #max fee check
         value = max_fee if value > max_fee else value
@@ -30,17 +30,51 @@ class Order(BaseModel):
             raise ValueError('only UTC zone is allowed.')
         return v
 
-    def _multiplicators(self, value):
+    def _multiplicators(self, value:int)->int:
+        value = self._multi_smallCart(value)
+        value = self._multi_distance(value)
+        value = self._multi_bulk(value)
         value = self._multi_rushTime(value)
         return value
 
-    def _multi_rushTime(self, value):
+    def _multi_smallCart(self, value:int)->int:
+        if self.cart_value < 1000:
+            value += 1000 - self.cart_value
+        return value
+
+    def _multi_distance(self, value:int)->int:
+        distance = self.delivery_distance - 1000
+        value+=200
+        while distance > 0:
+            value+=100
+            distance-=500
+        return value
+
+    def _multi_bulk(self, value:int)->int:
+        items = self.number_of_items
+        #fee for items
+        if items>=5:
+            items-=5
+            value+=50
+            value+=items*50
+        #bulk fee
+        if self.number_of_items > 12:
+            value += 120
+        return value
+
+    def _multi_rushTime(self, value:int)->int:
         print(type(self.time.tzinfo))
         print(self.time.tzname())
         if self.time.weekday() == 4:
             if time(15,0) <= self.time.time() < time(20,0):
-                return int(value * 1.2)
+                try:
+                    value = int(value * 1.2)
+                except:
+                    ...
         return value
+
+
+
 
 
 
